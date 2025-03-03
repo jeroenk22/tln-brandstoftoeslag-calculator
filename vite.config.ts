5fdf33ba9cf948e8b38e9d021717eb6769b29a49
Jeroen Krajenbrink <jeroenkrajenbrink@gmail.com>
2025-03-03 17:18:01 +0100

    Configuring

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,17 @@
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
+
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss()],
+  plugins: [react(), tailwindcss()],
+  base: "/tln-brandstoftoeslag-calculator/",
 });
+
+// import { defineConfig } from 'vite'
+// import tailwindcss from '@tailwindcss/vite'
+// export default defineConfig({
+//   plugins: [
+//     tailwindcss(),
+//   ],
+// })
